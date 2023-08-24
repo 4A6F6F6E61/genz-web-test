67e01e6aad31278cb6629ecedaf85e110f17b370
Joona Brückner <joona.brueckner@gmail.com>
2023-08-24 19:40:49 +0200

    readded vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
-    },
-  },
+  output: "server",
+  adapter: vercel(),
 });
